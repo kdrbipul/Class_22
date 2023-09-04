5e50ba898465edea4ef9f223662ff898f5578379
kdrbipul <kdrbipul@gmail.com>
2023-09-04 16:44:50 +0600

    first commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 function makeGreen(){
     document.body.style.backgroundColor = 'green';
 }
+
+const yellowBtn = document.getElementById('make-yellow');
+// console.log(yellowBtn);
+yellowBtn.onclick = makeYellow;
+function makeYellow(){
+    document.body.style.backgroundColor = 'yellow';
+}
+
+const goldenBtn = document.querySelector('#make-golden');
+// console.log(goldenBtn);
+goldenBtn.onclick = function(){
+    document.body.style.backgroundColor = "gold";
+}
